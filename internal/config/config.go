c920aec105785d5e35abecb8b1ab15bfad6c10b3
Ayush Verma <vermaayush6387@gmail.com>
2026-09-05 16:03:52 +0530

    Gracefull Shuttdown

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,7 +9,7 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address" env-required:"true`
 }
 
 // env-default:"production"
@@ -19,32 +19,32 @@ type Config struct {
 	HTTPServer  `yaml:"http_server"`
 }
 
-func MustLoad() *Config{
+func MustLoad() *Config {
 	var configPath string
 
 	configPath = os.Getenv("CONFIG_PATH")
 
 	if configPath == "" {
-		flags := flag.String("config","","path to the configuration file")
+		flags := flag.String("config", "", "path to the configuration file")
 
 		flag.Parse()
-		configPath= * flags
+		configPath = *flags
 
-		if configPath == ""{
+		if configPath == "" {
 			log.Fatal("Config path is not set")
 		}
 
 	}
 
-	if _,err:=os.Stat(configPath); os.IsNotExist(err){
-		log.Fatalf("config file does not exist: %s",configPath)
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
 	}
 
 	var cfg Config
 
-	err:=cleanenv.ReadConfig(configPath,&cfg)
-	if err!=nil{
-		log.Fatal("can not read config file: %s", err.Error())
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("can not read config file: %s", err.Error())
 	}
 	return &cfg
 }
